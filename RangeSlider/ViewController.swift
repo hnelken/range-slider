6b0035d611b0e87bdc2b3b768f41ebaaabd34d8d
Harry Nelken <harry.nelken@gmail.com>
2016-10-15 14:17:22 -0400

    tutorial result dump
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rangeSlider = RangeSlider(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        rangeSlider.addTarget(self, action: #selector(ViewController.rangeSliderValueChanged(_:)),
+                              for: .valueChanged)
+        view.addSubview(rangeSlider)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 20.0
+        let width = view.bounds.width - 2.0 * margin
+        rangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
+                                   width: width, height: 31.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
+        print("Range slider value changed: (\(rangeSlider.lowerValue) \(rangeSlider.upperValue))")
+    }
 }
 
